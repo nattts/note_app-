0fbc58a9da2d5cc498ea2d6bb2ab7fdc182d836a
nts <albicka@nts.local>
2017-10-05 05:27:49 +0100

    WTForms added. saving into DB functionality added

--- routes.py
+++ routes.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, redirect, url_for, request, jsonify
+from flask import Flask, render_template, redirect, url_for, request
 from datetime import datetime 
-import json
-import random
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import InputRequired, Length
 
- 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'top secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)   
 
@@ -17,21 +18,32 @@ class Note(db.Model):
 	text = db.Column(db.String(120))
 	date = db.Column(db.DateTime, default=datetime.utcnow)
 	
-
 	def __repr__(self):
 		return '<Note {}, Time {}>'.format(self.text, self.date)
+
+
+class Note_Form(Form):
+	add_note_button = SubmitField('add note')
+	edit_button = SubmitField('edit')
+	save_button = SubmitField('save')
+	delete_button = SubmitField('delete')
+	field = TextAreaField('input', validators=[InputRequired(), Length(min=1,max=50)])
 
 	
 @app.route('/',methods=['GET', 'POST'] )
 def index():
 	text = None
+	note_form = Note_Form()
 	notes = Note.query.all()
-	if request.method == 'POST' and request.form["submit"] == "add" :
-		text = request.form["input"]
-		db.session.add(Note(text=text))
-		db.session.commit()
-		return redirect(url_for('index'))
-	return render_template('index.html', notes=notes)
+	
+	if note_form.validate_on_submit():
+		if note_form.add_note_button.data:
+			text = note_form.field.data
+			db.session.add(Note(text=text))
+			db.session.commit()
+			return redirect(url_for('index'))
+		
+	return render_template('index.html', note_form=note_form, notes=notes)
 
 		
 			
